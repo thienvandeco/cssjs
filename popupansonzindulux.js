9e8c9809b50586ed07f4f7e4fd508f3e57b231cc
thienvandeco <thienvandeco@gmail.com>
2017-01-18 15:32:45 +0700

    Update popupansonzindulux.js

--- popupansonzindulux.js
+++ popupansonzindulux.js
@@ -1,8 +1,8 @@
 
-	var url_popup = 'http://bit.ly/1JdPEQv';
+	var url_popup = 'http://bit.ly/2k3nRS6';
 
       //<![CDATA[
-	  // var url_ppp = 'http://bit.ly/1KmPNYe';
+	  // var url_ppp = 'http://bit.ly/2k3nRS6';
       function addEvent(obj, eventName, func){
         if (obj.attachEvent)
         {
