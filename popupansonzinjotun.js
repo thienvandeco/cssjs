7f6fe790802909fb65e15ab3e3c0c3964a52e4e1
thienvandeco <thienvandeco@gmail.com>
2017-01-20 04:20:56 +0700

    Update popupansonzinjotun.js

--- popupansonzinjotun.js
+++ popupansonzinjotun.js
@@ -1,8 +1,8 @@
 
-	var url_popup = 'http://bit.ly/1JdPEQv';
+	var url_popup = 'http://bit.ly/2irjMlev';
 
       //<![CDATA[
-	  // var url_ppp = 'http://bit.ly/1KmPNYe';
+	  // var url_ppp = 'http://bit.ly/2irjMle';
       function addEvent(obj, eventName, func){
         if (obj.attachEvent)
         {
